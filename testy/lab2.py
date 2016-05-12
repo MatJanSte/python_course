a6bca03b3a87257422d35a9c76cbc89f9ef61393
mathew <artszol@wp.pl>
2016-05-12 22:14:08 +0200

    początek drugich lab

---
+++ testy/lab2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import lab2a
+def funkcja(argument1):
+    """
+
+    :param argument1: #zmienna
+    :return:
+    """
+    a = argument1[3]
+    return a
+print funkcja("to jest string")
+
+lab2a.gaugan("to jest liczba nie wierz mu")
